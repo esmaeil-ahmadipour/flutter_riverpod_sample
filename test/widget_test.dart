e80fdd23141ebccc648df908da5f9b95f55cf9b8
esmaeil-ahmadipour <software8686@gmail.com>
2024-07-26 17:49:44 +0330

    test(app): write widget test for counter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,46 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_sample/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+// Make sure WidgetsFlutterBinding is initialized
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+// Simulate SharedPreferences for testing
+  SharedPreferences.setMockInitialValues({});
+
+// Initialize EasyLocalization
+  await EasyLocalization.ensureInitialized();
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+// Create widget inside ProviderScope and EasyLocalization
+    await tester.pumpWidget(
+      ProviderScope(
+        child: EasyLocalization(
+          supportedLocales: languages,
+          path: 'assets/localizations',
+          fallbackLocale: const Locale('en', 'US'),
+          useFallbackTranslations: true,
+          child: const MyApp(),
+        ),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
+// Waiting for initial rendering
+    await tester.pumpAndSettle();
+
+    // Make sure counter is initialized to 0
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // Click the '+' button and render a new frame
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
+// Make sure the counter is incremented
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
